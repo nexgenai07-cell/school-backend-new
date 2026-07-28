c5e6a60f40460549a6f3eb4b508cc2fa2142798b
mominaabid <mominaabid789@gmail.com>
2026-07-28 14:18:55 +0500

    password

--- apps/users/admin.py
+++ apps/users/admin.py
@@ -1,7 +1,44 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 from .models import User, Student, Teacher, Staff, Parent
 
-admin.site.register(User)
+
+class CustomUserCreationForm(UserCreationForm):
+    class Meta(UserCreationForm.Meta):
+        model = User
+        fields = ('email', 'name', 'role')
+
+
+class CustomUserChangeForm(UserChangeForm):
+    class Meta(UserChangeForm.Meta):
+        model = User
+        fields = '__all__'
+
+
+class UserAdmin(BaseUserAdmin):
+    add_form = CustomUserCreationForm
+    form = CustomUserChangeForm
+    model = User
+
+    list_display = ('email', 'name', 'role', 'status', 'is_staff')
+    ordering = ('email',)
+    search_fields = ('email', 'name')
+
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        ('Personal Info', {'fields': ('name', 'role', 'status')}),
+        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'name', 'role', 'password1', 'password2'),
+        }),
+    )
+
+
+admin.site.register(User, UserAdmin)
 admin.site.register(Student)
 admin.site.register(Teacher)
 admin.site.register(Staff)
